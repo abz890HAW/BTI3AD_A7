e87273d5aeabcd973caedb522d8682c33a9dbdf3
marble <markusblechschmidt@gmail.com>
2017-05-10 14:26:22 +0200

    implemented insert

--- src/BinTree/BinListTree/BinListTree.java
+++ src/BinTree/BinListTree/BinListTree.java
@@ -12,7 +12,25 @@ public class BinListTree implements IBinTree {
         comparables = new Comparable[(int)Math.pow(2, depth)];
     }
 
+    private int leftOf(int idx) {
+        return 2*idx;
+    }
+
+    private int rightOf(int idx) {
+        return 2*idx+1;
+    }
+
     public boolean insert(Comparable comparable) {
+        int idx = 0;
+        while(null != comparables[idx]) {
+            int comparison = comparables[idx].compareTo(comparable);
+            /* check if element is already in tree */
+            if(0 == comparison) {
+                return false;
+            }
+            idx = (comparison < 0) ? leftOf(idx) : rightOf(idx);
+        }
+        comparables[0] = comparable;
         return true;
     }
 
